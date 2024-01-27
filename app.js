9ff52ea7b7614edcfc69735e06ac3d14bf4155e4
kepop <dpopek135@gmail.com>
2024-01-27 21:55:24 +0100

    fabuła + naprawione doTheEvent

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ let data = {
 			AgreSuS: 50,
 		},
 	},
+	notimeleft: false,
 	areTeethBrushed : false,
 	isCoffeDrunk : false,
 	isBreakfastEaten : false
@@ -78,24 +79,28 @@ function doTheEvent(ob) {
 	newBigPhoto.setAttribute("src", ob.photoPath);
 	middlediv.insertAdjacentElement('beforeend', newBigPhoto);
 	newBigPhoto.classList.add("bigPhoto");
+
+	let whoinfo = document.createElement("div")
+	whoinfo.classList.add("whoInfo");
+	newBigPhoto.insertAdjacentElement('afterend', whoinfo)
 	
 	ob.line.forEach((element) => {
 		let newPhoto = document.createElement("img");
 		newPhoto.setAttribute("src", element.photoPath);
-		middlediv.insertAdjacentElement("beforeend", newPhoto);
+		whoinfo.insertAdjacentElement("beforeend", newPhoto);
 		newPhoto.classList.add("linePhoto");
 
 		let newSpanNode = document.createTextNode(element.who);
 		let newSpan = document.createElement("span");
 		newSpan.appendChild(newSpanNode);
 		newSpan.style.color = element.color;
-		middlediv.insertAdjacentElement("beforeend", newSpan);
+		newPhoto.insertAdjacentElement("afterend", newSpan);
 		newSpan.classList.add("lineWho");
 
 		let newTextNode = document.createTextNode(element.text);
 		let newText = document.createElement("span");
 		newText.appendChild(newTextNode);
-		middlediv.insertAdjacentElement("beforeend", newText);
+		newSpan.insertAdjacentElement("afterend", newText);
 		newText.classList.add("lineText");
 
 		middlediv.insertAdjacentElement(
@@ -147,15 +152,19 @@ let c1u1e1 = new MyEvent(
 		"Zaraz się spóźnię!",
 		"Nie mam czasu, muszę się zbierać",
 		function () {
-			console.log("leftoption");
+			notimeleft = true;
+
+			doTheEvent(c1u1e3v1);
 		},
 		function () {
-			console.log("rightoption");
+			notimeleft = true;
+
+			doTheEvent(c1u1e3v1);
 		}
 	);
 
 	let c1u1e2v2 = new MyEvent(
-		"bogosBinted/rysunek.svg",
+		"bogosBinted/rysunek.svg", //budzik.svg
 		[new MyLine(
 			"bogosBinted/rysunek.svg", "Jakub", "green",
 			"Na dzisiejszym wywiadzie muszę się dobrze prezentować! Ale mogę nie zdążyć ze wszystkim."
@@ -165,32 +174,58 @@ let c1u1e1 = new MyEvent(
 		function (){
 			data.isCoffeDrunk = true;
 	
-			//nextOptionToRun
+			if(notimeleft==true) {}
+			else doTheEvent(c1u1e3v1)
 		},
 		function (){
 			data.areTeethBrushed = true;
 	
-			//nextOptionToRun
+			if(notimeleft=true) {}
+			else doTheEvent(c1u1e3v2)
 		}
 	)
 
 	let c1u1e3v1 = new MyEvent(
-		"bogosBinted/rysunek.svg",
-		[MyLine(
-			"bogosBinted/rysunek.svg", "Jakub", "red",
-			"testline"
+		"bogosBinted/rysunek.svg", //kawa.svg
+		[new MyLine(
+			"bogosBinted/rysunek.svg", "Jakub", "green",
+			"Dzień bez kawy to dzień stracony! Dobra, na szczęscie zostało mi trochę czasu na..."
 		)],
-		"LewaOpcja",
-		"PrawaOpcja",
+		"Wymycie zębów",
+		"Szbykie śniadanie",
 		function (){
-			//stats
+			data.areTeethBrushed = true;
 	
 			//nextOptionToRun
 	
 			console.log("OpcjaL")
 		},
 		function (){
-			//stats
+			data.isBreakfastEaten = true;
+	
+			//nextOptionToRun
+	
+			console.log("OpcjaP")
+		}
+	)
+
+	let c1u1e3v2 = new MyEvent(
+		"bogosBinted/rysunek.svg", //zeby.svg
+		[new MyLine(
+			"bogosBinted/rysunek.svg", "Jakub", "green",
+			"No, i takie ząbki ma człowiek sukcesu :DD teraz zostało mi jeszcze trochę czasu żeby .."
+		)],
+		"Zjeść śniadanie",
+		"Wypić kawę",
+		function (){
+			data.isBreakfastEaten = true
+	
+			//nextOptionToRun
+	
+			console.log("OpcjaL")
+		},
+		function (){
+			data.isCoffeDrunk = true
 	
 			//nextOptionToRun
 	
@@ -202,7 +237,7 @@ doTheEvent(c1u1e1);
 /*
 let cxuxexvx = new MyEvent(
 	"bogosBinted/rysunek.svg",
-	[MyLine(
+	[new MyLine(
 		"bogosBinted/rysunek.svg", "Jakub", "red",
 		"testline"
 	)],
